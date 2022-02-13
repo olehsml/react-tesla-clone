1864dbbb0edb9cd70d316f55eb3122a6acadba48
OlehS <olehjs16@gmail.com>
2022-02-13 22:44:28 +0200

    Create a React app and deploy it to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
 import Header from './components/Header';
 import Home from './components/Home';
 
