6e03ece0ed9ea20f42a180040424578b54094660
OlehS <olehjs16@gmail.com>
2022-02-13 20:30:08 +0200

    ADD: Sections

---
+++ src/components/Home.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import styled from "styled-components"
+import Section from './Section'
+
+function Home() {
+    return (
+        <Container>
+            <Section
+                title="Model S"
+                description="Order Online for Touchless delivery"
+                backgroundImg="model-s.jpg"
+                leftBtnText="Custom order"
+                rightBtnText="Existing inventory"
+            />
+            <Section
+                title="Model Y"
+                description="Order Online for Touchless delivery"
+                backgroundImg="model-y.jpg"
+                leftBtnText="Custom order"
+                rightBtnText="Existing inventory"
+            />
+            <Section
+                title="Model 3"
+                description="Order Online for Touchless delivery"
+                backgroundImg="model-3.jpg"
+                leftBtnText="Custom order"
+                rightBtnText="Existing inventory"
+            />
+            <Section
+                title="Model X"
+                description="Order Online for Touchless delivery"
+                backgroundImg="model-x.jpg"
+                leftBtnText="Custom order"
+                rightBtnText="Existing inventory"
+            />
+            <Section
+                title="Solar Panels"
+                description="Lowest Cost Solar Panels in America"
+                backgroundImg="solar-panel.jpg"
+                leftBtnText="Order now"
+                rightBtnText="Learn more"
+            />
+            <Section
+                title="Solar Roof"
+                description="Produce Clean Energy From Your Roof"
+                backgroundImg="solar-roof.jpg"
+                leftBtnText="Order now"
+                rightBtnText="Learn more"
+            />
+            <Section
+                title="Accessories"
+                backgroundImg="accessories.jpg"
+                leftBtnText="Shop now"
+            />
+        </Container>
+    )
+}
+
+export default Home
+
+const Container = styled.div`
+    height: 100vh;
+`
